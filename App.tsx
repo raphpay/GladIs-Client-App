3e45893f2409bf042b6429ea06574e9c04649a24
raphpay <raphpay.dev@gmail.com>
2024-01-30 15:32:40 +0100

    Feature: integrate i18n and first translations #2

--- App.tsx
+++ App.tsx
@@ -1,26 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import { SafeAreaView, StyleSheet, useColorScheme } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import DashboardScreen from './src/ui/screens/dashboard/DashboardScreen';
+import './src/localization/i18n';
+
+import { Colors } from './src/ui/components/colors';
+import LoginScreen from './src/ui/screens/LoginScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.dark : Colors.light,
   };
 
   return (
     <SafeAreaView style={[backgroundStyle, styles.container]}>
-      {/* <LoginScreen /> */}
-      <DashboardScreen isAdmin={false} />
+      <LoginScreen />
+      {/* <DashboardScreen isAdmin={false} /> */}
     </SafeAreaView>
   );
 }
