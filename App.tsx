b27c2e07e0133c847dc58bd361fb71fa41551058
raphpay <raphpay.dev@gmail.com>
2024-02-07 22:05:41 +0100

    Feature: Force light mode #6

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Appearance } from 'react-native';
 import './src/localization/i18n';
-
 import { Routes } from './src/navigation/Routes';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+     Appearance.setColorScheme('light');
+  }, []);
+
   return ( <Routes /> );
 }
 
