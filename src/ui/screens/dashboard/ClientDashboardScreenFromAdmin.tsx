7ce8803fb3e112225c38cfe64813cfe7875bb0dd
raphpay <raphpay.dev@gmail.com>
2024-03-04 16:17:05 +0100

    Refactor: Clean component #24

--- src/ui/screens/dashboard/ClientDashboardScreenFromAdmin.tsx
+++ src/ui/screens/dashboard/ClientDashboardScreenFromAdmin.tsx
@@ -1,28 +1,21 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { IRootStackParams } from '../../../navigation/Routes';
 
 import INavigationHistoryItem from '../../../business-logic/model/INavigationHistoryItem';
-import CacheKeys from '../../../business-logic/model/enums/CacheKeys';
 import NavigationRoutes from '../../../business-logic/model/enums/NavigationRoutes';
-import UserType from '../../../business-logic/model/enums/UserType';
-import CacheService from '../../../business-logic/services/CacheService';
-import UserService from '../../../business-logic/services/UserService';
 
 import AppContainer from '../../components/AppContainer';
 import DashboardClientFlatList from '../../components/DashboardClientFlatList';
 import ErrorDialog from '../../components/ErrorDialog';
-import IconButton from '../../components/IconButton';
 
 type ClientDashboardScreenFromAdminProps = NativeStackScreenProps<IRootStackParams, NavigationRoutes.ClientDashboardScreenFromAdmin>;
 
 function ClientDashboardScreenFromAdmin(props: ClientDashboardScreenFromAdminProps): any {
   const [searchText,setSearchText] = useState<string>('');
-  const [isAdmin, setIsAdmin] = useState<boolean>(false);
   const [showDialog, setShowDialog] = useState<boolean>(false);
-  const plusIcon = require('../../assets/images/plus.png');
   const { navigation } = props;
   const { t } = useTranslation();
   const navigationHistoryItems: INavigationHistoryItem[] = [
@@ -32,22 +25,9 @@ function ClientDashboardScreenFromAdmin(props: ClientDashboardScreenFromAdminPro
     }
   ]
 
-  function navigateToClientList() {
-    navigation.navigate(NavigationRoutes.ClientManagementStack);
-  }
-
   function navigateBack() {
     navigation.goBack();
   }
-
-  useEffect(() => {
-     async function init() {
-      const userID = await CacheService.getInstance().retrieveValue(CacheKeys.currentUserID) as string;
-      const user = await UserService.getInstance().getUserByID(userID);
-      setIsAdmin(user.userType == UserType.Admin);
-    }
-    init();
-  }, []);
 
   return (
     <>
@@ -60,15 +40,6 @@ function ClientDashboardScreenFromAdmin(props: ClientDashboardScreenFromAdminPro
         showSearchText={true}
         navigateBack={navigateBack}
         dialogIsShown={showDialog}
-        adminButton={(
-          isAdmin ? (
-            <IconButton
-              title={t('components.buttons.addClient')}
-              icon={plusIcon}
-              onPress={navigateToClientList}
-            />
-          ) : undefined
-        )}
       >
         <DashboardClientFlatList searchText={searchText} setShowDialog={setShowDialog} />
       </AppContainer>
