d516f112a19cf7daa8b396ae1c48333af1016a27
raphpay <raphpay.dev@gmail.com>
2024-03-11 17:01:16 +0100

    Doc: Continue the busniess-logic documentation #29

--- src/business-logic/store/store.ts
+++ src/business-logic/store/store.ts
@@ -3,6 +3,9 @@ import appStateReducer from './slices/appStateReducer';
 import tokenReducer from './slices/tokenReducer';
 import userReducer from './slices/userReducer';
 
+/**
+ * The Redux store instance.
+ */
 const store = configureStore({
   reducer: {
     appState: appStateReducer,
@@ -13,8 +16,19 @@ const store = configureStore({
 
 export default store;
 
+/**
+ * The dispatch function type for the Redux store.
+ */
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>
+
+/**
+ * The root state type for the Redux store.
+ */
+export type RootState = ReturnType<typeof store.getState>;
+
+/**
+ * The type for asynchronous actions in the Redux store.
+ */
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
--- src/business-logic/utils/Utils.ts
+++ src/business-logic/utils/Utils.ts
@@ -1,14 +1,28 @@
+
+/**
+ * Utility class containing helper methods for common operations.
+ */
 class Utils {
+  /**
+   * Checks if a phone number is valid.
+   * @param input - The phone number to validate.
+   * @returns A boolean indicating whether the phone number is valid or not.
+   */
   static isPhoneValid(input: string): boolean {
     const regex: RegExp = /^(0|\+33|0033)[1-9]([-. ]?[0-9]{2}){4}$/;
     const isValid: boolean = regex.test(input);
     return isValid;
   }
   
+  /**
+   * Checks if an email address is valid.
+   * @param input - The email address to validate.
+   * @returns A boolean indicating whether the email address is valid or not.
+   */
   static isEmailValid(input: string): boolean {
     const regex: RegExp = /[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}/;
     const isValid: boolean = regex.test(input);
-    return isValid ;
+    return isValid;
   }
 }
 
