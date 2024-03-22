6011652c7ca47fe7b01051c6b4a0ca1682cbc9fa
raphpay <raphpay.dev@gmail.com>
2024-03-22 18:30:02 +0100

    Feature: Add cache to client icon #40

--- src/ui/components/AppIcon.tsx
+++ src/ui/components/AppIcon.tsx
@@ -7,11 +7,12 @@ import {
   ViewStyle
 } from 'react-native';
 
-
 import PlatformName from '../../business-logic/model/enums/PlatformName';
+import CacheService from '../../business-logic/services/CacheService';
 import DocumentService from '../../business-logic/services/DocumentService';
 import { useAppSelector } from '../../business-logic/store/hooks';
 import { RootState } from '../../business-logic/store/store';
+
 import styles from '../assets/styles/components/AppIconStyles';
 
 type AppIconProps = {
@@ -27,18 +28,36 @@ function AppIcon(props: AppIconProps): React.JSX.Element {
   const { currentClient } = useAppSelector((state: RootState) => state.users);
   const { token } = useAppSelector((state: RootState) => state.tokens);
 
-  async function loadLogo() {
+  async function loadLogoFromAPI() {
     if (currentClient) {
       const company = currentClient.companyName;
       const docs = await DocumentService.getInstance().getDocumentsAtPath(`${company}/logos/`, token);
       const logo = docs[0];
       if (logo && logo.id) {
         const logoData = await DocumentService.getInstance().download(logo.id, token);
-        Platform.OS === PlatformName.Mac ? setLogoURI(`data:image/png;base64,${logoData}`) :  setLogoURI(logoData);
+        await CacheService.getInstance().storeValue(`${currentClient.id}/logo`, logoData)
+        displayLogo(logoData);
       }
     } else {
       setLogoURI('');
     }
+  }
+
+  async function loadLogo() {
+    if (currentClient) {
+      const cachedLogo = await CacheService.getInstance().retrieveValue(`${currentClient.id}/logo`);
+      if (cachedLogo) {
+        displayLogo(cachedLogo as string);
+      } else {
+        await loadLogoFromAPI();
+      }
+    } else {
+      setLogoURI('');
+    }
+  }
+
+  async function displayLogo(data: string) {
+    Platform.OS === PlatformName.Mac ? setLogoURI(`data:image/png;base64,${data}`) : setLogoURI(data);
   }
 
   useEffect(() => {
