521ae0690e3a6519c948e58ebd961ffb6eee9763
raphpay <raphpay.dev@gmail.com>
2024-03-24 15:07:29 +0100

    Feature: Place pagination in the middle of the screen #41

--- src/ui/assets/styles/components/PaginationStyles.ts
+++ src/ui/assets/styles/components/PaginationStyles.ts
@@ -2,10 +2,16 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 10,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 10,
+    left: 0,
+    zIndex: -1
   },
   pageNumber: {
     marginHorizontal: 8
