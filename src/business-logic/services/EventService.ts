da603af8552d772459e77b0dc51a7312c03b687e
raphpay <raphpay.dev@gmail.com>
2024-04-05 15:56:10 +0200

    Doc: add documentation #42

--- src/business-logic/services/EventService.ts
+++ src/business-logic/services/EventService.ts
@@ -53,6 +53,13 @@ class EventService {
     }
   }
 
+  /**
+   * Restores the event with the specified ID.
+   * @param eventID - The ID of the event to restore.
+   * @param token - The authentication token.
+   * @returns A promise that resolves when the event is restored.
+   * @throws If an error occurs while restoring the event.
+   */
   async restore(eventID: string, token: IToken | null): Promise<void> {
     try {
       const url = `${this.baseRoute}/restore/${eventID}`;
