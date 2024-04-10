5dd3f5b73ee5c8b3d0dc4c4031946d13caf4ece7
raphpay <raphpay.dev@gmail.com>
2024-04-10 19:04:54 +0200

    Feature: Better handle expanding text input #32

--- src/ui/assets/styles/components/ExpandingTextInputStyles.ts
+++ src/ui/assets/styles/components/ExpandingTextInputStyles.ts
@@ -1,19 +1,18 @@
 import { StyleSheet } from 'react-native';
 import { Colors } from '../../colors/colors';
-import { Fonts } from '../../fonts/fonts';
 
 const styles = StyleSheet.create({
   inputContainer: {
     width: '100%',
+    padding: 2,
   },
   input: {
-    textAlignVertical: 'center',
     width: '100%',
     borderWidth: 1,
     borderColor: Colors.black,
     borderRadius: 10,
-    paddingHorizontal: 15,
-    fontSize: Fonts.p
+    padding: 10,
+    fontSize: 16,
   },
 });
 
