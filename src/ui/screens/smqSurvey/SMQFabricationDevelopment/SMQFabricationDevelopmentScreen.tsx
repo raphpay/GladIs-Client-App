ff1285b6a72f07338fa6c0aaed672c3482915856
raphpay <raphpay.dev@gmail.com>
2024-04-22 17:48:39 +0200

    Feature: Add translations and fields to next step #50

--- src/ui/screens/smqSurvey/SMQFabricationDevelopment/SMQFabricationDevelopmentScreen.tsx
+++ src/ui/screens/smqSurvey/SMQFabricationDevelopment/SMQFabricationDevelopmentScreen.tsx
@@ -1,24 +1,31 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { ScrollView } from 'react-native';
 import NavigationRoutes from '../../../../business-logic/model/enums/NavigationRoutes';
 import { ISMQSurveyParams } from '../../../../navigation/Routes';
 import AppContainer from '../../../components/AppContainer/AppContainer';
+import GladisTextInput from '../../../components/TextInputs/GladisTextInput';
 
 type SMQFabricationDevelopmentScreenProps = NativeStackScreenProps<ISMQSurveyParams, NavigationRoutes.SMQFabricationDevelopmentScreen>;
 
 function SMQFabricationDevelopmentScreen(props: SMQFabricationDevelopmentScreenProps): React.JSX.Element {
 
   const { navigation } = props;
+  const { t } = useTranslation();
+  // States
+  const [processusPilotName, setProcessusPilotName] = React.useState<string>('');
+  const [productionFlux, setProductionFlux] = React.useState<string>('');
+  const [productIdentifications, setProductIdentifications] = React.useState<string>('');
+  const [productPreservation, setProductPreservation] = React.useState<string>('');
+  const [productTracking, setProductTracking] = React.useState<string>('');
 
   // Sync Methods
   function navigateBack() {
     navigation.goBack();
   }
 
-  async function tappedContinue() {
-    console.log('tappedContinue');
-  }
-
+  // Components
   return (
     <AppContainer
       mainTitle='Fabrication Development'
@@ -27,8 +34,38 @@ function SMQFabricationDevelopmentScreen(props: SMQFabricationDevelopmentScreenP
       showBackButton={true}
       navigateBack={navigateBack}
     >
-      <>
-      </>
+      <ScrollView>
+        <GladisTextInput
+          value={processusPilotName}
+          onValueChange={setProcessusPilotName}
+          placeholder={t('smqSurvey.prs.management.processusPilotName')}
+          showTitle={true}
+        />
+        <GladisTextInput
+          value={productionFlux}
+          onValueChange={setProductionFlux}
+          placeholder={t('smqSurvey.prs.fabricationDevelopment.productionFlux')}
+          showTitle={true}
+        />
+        <GladisTextInput
+          value={productIdentifications}
+          onValueChange={setProductIdentifications}
+          placeholder={t('smqSurvey.prs.fabricationDevelopment.productIdentifications')}
+          showTitle={true}
+        />
+        <GladisTextInput
+          value={productPreservation}
+          onValueChange={setProductPreservation}
+          placeholder={t('smqSurvey.prs.fabricationDevelopment.productPreservation')}
+          showTitle={true}
+        />
+        <GladisTextInput
+          value={productTracking}
+          onValueChange={setProductTracking}
+          placeholder={t('smqSurvey.prs.fabricationDevelopment.productTracking')}
+          showTitle={true}
+        />
+      </ScrollView>
     </AppContainer>
   );
 }
