af3155ec8ca9d7d840c63f605d31f77c29e1122f
raphpay <raphpay.dev@gmail.com>
2024-04-29 14:14:58 +0200

    Feature: Load windows packages #51

---
+++ windows/GladIs/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::GladIs::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::GladIs::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
