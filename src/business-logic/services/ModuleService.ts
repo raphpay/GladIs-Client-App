98b0085395e5445e1234c4ae06be5cc8a40063ac
raphpay <raphpay.dev@gmail.com>
2024-05-21 16:04:41 +0200

    Refactor: Clean Module Service #54

--- src/business-logic/services/ModuleService.ts
+++ src/business-logic/services/ModuleService.ts
@@ -1,13 +1,10 @@
 import IModule from '../model/IModule';
-import IToken from '../model/IToken';
-import APIService from './APIService';
 
 /**
  * Represents a service for managing modules.
  */
 class ModuleService {
   private static instance: ModuleService | null = null;
-  private baseRoute = 'modules';
   private modules: IModule[] = [
     {
       id: '1',
@@ -50,36 +47,6 @@ class ModuleService {
    */
   getModules(): IModule[] {
     return this.modules
-  }
-
-  /**
-   * Retrieves all modules sorted by their order.
-   * @returns A promise that resolves to an array of modules sorted by their order.
-   * @throws If there was an error retrieving the modules.
-   */
-  async getSortedModules(token: IToken | null): Promise<IModule[]> {
-    try {
-      const modules = await APIService.get<IModule[]>(`${this.baseRoute}/sorted`, token?.value as string);
-      return modules;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  /**
-   * Retrieves a module by its ID.
-   * @param id - The ID of the module to retrieve.
-   * @returns A promise that resolves to the module with the specified ID.
-   * @throws If there was an error retrieving the module.
-   */
-  async getModuleByID(id: string): Promise<IModule> {
-    try {
-      const module = await APIService.get<IModule>(`${this.baseRoute}/${id}`);
-      return module;
-    } catch (error) {
-      console.log('Error getting modules', error);
-      throw error;
-    }
   }
 }
 
