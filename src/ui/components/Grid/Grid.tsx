1b28181f2ce4bb9bed6307b7593fe6c20dd87e0c
raphpay <raphpay.dev@gmail.com>
2024-09-17 14:11:54 +0400

    Fix: Correct ScrollView on Windows #51

--- src/ui/components/Grid/Grid.tsx
+++ src/ui/components/Grid/Grid.tsx
@@ -1,6 +1,8 @@
 import type { ListRenderItem } from '@react-native/virtualized-lists';
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { Platform, ScrollView, View } from 'react-native';
+
+import PlatformName from '../../../business-logic/model/enums/PlatformName';
 
 import styles from '../../assets/styles/components/GridStyles';
 
@@ -9,10 +11,9 @@ type GridProps<ItemT> = {
   keyExtractor?: ((item: ItemT, index: number) => string) | undefined;
   renderItem: ListRenderItem<ItemT> | null | undefined;
   scrollEnabled?: boolean;
-}
+};
 
 function Grid<ItemT>(props: GridProps<ItemT>): React.JSX.Element {
-
   const {
     data,
     keyExtractor,
@@ -20,22 +21,63 @@ function Grid<ItemT>(props: GridProps<ItemT>): React.JSX.Element {
     scrollEnabled = true,
   } = props;
 
-  return (
-    <ScrollView
-      contentContainerStyle={styles.grid}
-      scrollEnabled={scrollEnabled}
-    >
-      {
-        data && renderItem && data.map((item: ItemT, index: number) => {
+  function WindowsComponent() {
+    return (
+      <View>
+        {data && renderItem && data.map((item: ItemT, index: number) => {
           const key = keyExtractor ? keyExtractor(item, index) : String(index);
           return (
             <React.Fragment key={key}>
-              {renderItem({ item, index, separators: { highlight: () => {}, unhighlight: () => {}, updateProps: (select: 'leading' | 'trailing', newProps: any) => {} } })}
+              {renderItem({
+                item,
+                index,
+                separators: {
+                  highlight: () => {},
+                  unhighlight: () => {},
+                  updateProps: (select: 'leading' | 'trailing', newProps: any) => {},
+                },
+              })}
             </React.Fragment>
           );
-        })
-      }     
-    </ScrollView>
+        })}
+      </View>
+    );
+  }
+
+  function AllPlatformsComponent() {
+    return (
+      <ScrollView
+        contentContainerStyle={styles.grid}
+        scrollEnabled={scrollEnabled}
+      >
+        {data && renderItem && data.map((item: ItemT, index: number) => {
+          const key = keyExtractor ? keyExtractor(item, index) : String(index);
+          return (
+            <React.Fragment key={key}>
+              {renderItem({
+                item,
+                index,
+                separators: {
+                  highlight: () => {},
+                  unhighlight: () => {},
+                  updateProps: (select: 'leading' | 'trailing', newProps: any) => {},
+                },
+              })}
+            </React.Fragment>
+          );
+        })}
+      </ScrollView>
+    );
+  }
+
+  return (
+    <>
+      {Platform.OS === PlatformName.Windows ? (
+        <WindowsComponent />
+      ) : (
+        <AllPlatformsComponent />
+      )}
+    </>
   );
 }
 
