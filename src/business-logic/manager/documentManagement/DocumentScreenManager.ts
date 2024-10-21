da749efa0c57693ec924adbd6a5b4f432d813c43
raphpay <raphaelpayet@icloud.com>
2024-10-21 15:48:39 +0400

    Doc: Add documentation #69

--- src/business-logic/manager/documentManagement/DocumentScreenManager.ts
+++ src/business-logic/manager/documentManagement/DocumentScreenManager.ts
@@ -21,6 +21,17 @@ class DocumentScreenManager {
     return DocumentScreenManager.instance;
   }
 
+  /**
+   * Uploads a file to an API by specifying the origin path, file name, and destination path.
+   * The token is used for authentication during the upload process.
+   *
+   * @param originPath - The file's path on the local device.
+   * @param fileName - The name of the file to upload.
+   * @param destinationPath - The path on the server where the file will be uploaded.
+   * @param token - The authentication token for the API (can be null).
+   * @returns A promise that resolves to an `IDocument` object representing the uploaded file.
+   * @throws Will throw an error if the upload fails.
+   */
   async uploadFileToAPI(
     originPath: string,
     fileName: string,
@@ -40,6 +51,18 @@ class DocumentScreenManager {
     }
   }
 
+  /**
+   * Records an activity log for a document, tracking user actions such as viewing, editing, or sharing a document.
+   * The log contains details such as the action performed, the user acting, and whether the user is an admin.
+   *
+   * @param action - The action performed on the document (e.g., view, edit, download).
+   * @param currentUser - The user currently performing the action.
+   * @param currentClient - The client associated with the action (if applicable).
+   * @param documentID - The ID of the document being logged.
+   * @param token - The authentication token for the API (can be null).
+   * @param actorIsAdmin - Optional flag indicating if the actor is an admin (if null or undefined, the method will infer this from the currentUser's userType).
+   * @returns A promise that resolves when the document activity log is successfully recorded.
+   */
   async recordDocumentActivity(
     action: DocumentLogAction,
     currentUser: IUser | undefined,
@@ -47,7 +70,7 @@ class DocumentScreenManager {
     documentID: string,
     token: IToken | null,
     actorIsAdmin?: boolean | null,
-  ) {
+  ): Promise<void> {
     const logInput: IDocumentActivityLogInput = {
       action,
       actorIsAdmin: actorIsAdmin || currentUser?.userType == UserType.Admin,
--- src/business-logic/manager/documentManagement/PDFScreenManager.ts
+++ src/business-logic/manager/documentManagement/PDFScreenManager.ts
@@ -19,13 +19,29 @@ class PDFScreenManager {
     return PDFScreenManager.instance;
   }
 
-  async cacheDownloadedData(originalDocument: IDocument, data: string[]) {
+  /**
+   * Caches the downloaded PDF data by storing it with a unique document ID.
+   * @param originalDocument - The original document object containing the document ID.
+   * @param data - The array of strings representing the base64 encoded PDF pages.
+   * @returns A promise that resolves when the data is successfully cached.
+   */
+  async cacheDownloadedData(
+    originalDocument: IDocument,
+    data: string[],
+  ): Promise<void> {
     await CacheService.getInstance().storeValue(
       originalDocument.id as string,
       data,
     );
   }
 
+  /**
+   * Downloads a document by its ID using the provided token.
+   * @param id - The ID of the document to download.
+   * @param token - The authentication token (can be null).
+   * @returns A promise that resolves to the downloaded document data as a string.
+   * @throws Will throw an error if the download fails.
+   */
   async downloadDocument(id: string, token: IToken | null): Promise<string> {
     try {
       const data = await DocumentServiceGet.download(id, token);
@@ -35,6 +51,12 @@ class PDFScreenManager {
     }
   }
 
+  /**
+   * Splits a PDF document into individual pages and returns each page as a base64 encoded string.
+   * @param originalData - The base64 encoded string of the original PDF document.
+   * @returns A promise that resolves to an array of base64 encoded strings, where each string represents a single page of the PDF.
+   * @throws Will throw an error if splitting the PDF fails.
+   */
   async splitPDF(originalData: string): Promise<string[]> {
     let data: string[] = [];
     try {
@@ -44,7 +66,8 @@ class PDFScreenManager {
         const newPdfDoc = await PDFDocument.create();
         const [copiedPage] = await newPdfDoc.copyPages(pdfDoc, [i]);
         newPdfDoc.addPage(copiedPage);
-        // Serialize the new PDF to bytes
+
+        // Serialize the new PDF to a base64 encoded string
         const newPdfBytes = await newPdfDoc.saveAsBase64();
         data.push(newPdfBytes);
       }
