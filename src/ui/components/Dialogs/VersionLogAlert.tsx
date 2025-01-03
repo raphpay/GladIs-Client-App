61728b80eeacbfc3ea4fe24b065df93e1d458e8f
raphpay <raphpay.dev@gmail.com>
2025-01-03 17:33:02 +0100

    Feature: Open app store #143

--- src/ui/components/Dialogs/VersionLogAlert.tsx
+++ src/ui/components/Dialogs/VersionLogAlert.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Linking, Platform } from 'react-native';
 
+import PlatformName from '../../../business-logic/model/enums/PlatformName';
+import {
+  ANDROID_APP_LINK,
+  WINDOWS_APP_LINK,
+} from '../../../business-logic/utils/envConfig';
 import Dialog from './Dialog';
 
 type VersionLogAlertProps = {
@@ -8,9 +14,21 @@ type VersionLogAlertProps = {
 
 function VersionLogAlert(props: VersionLogAlertProps): React.JSX.Element {
   const { show } = props;
-  function goToAppStore() {
-    // TODO: Implement the logic to redirect to the app store
-    console.log('goToAppStore');
+
+  async function goToAppStore() {
+    // Checking if the link is supported for links with custom URL scheme.
+    let url: string = '';
+    if (Platform.OS === PlatformName.Android) {
+      url = ANDROID_APP_LINK;
+    } else if (Platform.OS === PlatformName.Windows) {
+      url = WINDOWS_APP_LINK;
+    }
+
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      await Linking.openURL(url);
+    }
   }
 
   return (
